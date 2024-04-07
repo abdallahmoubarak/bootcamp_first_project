24f191d676e46a8974af472050da1508ef86b1f0
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2024-04-07 21:47:55 +0300

    updating signup form

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,26 @@
 export default function Home() {
   return (
     <div className="flex flex-col max-w-sm mx-auto gap-3">
-      <h1 className="text-center text-4xl text-red-600 font-bold">
-        Hello World
-      </h1>
+      <h1 className="text-center text-4xl text-red-600 font-bold">Sign Up</h1>
       <input
         className="border border-red-600 rounded-lg p-2 "
         type="text"
-        placeholder="First Name"
+        placeholder="Full Name"
+      />
+      <input
+        className="border border-red-600 rounded-lg p-2 "
+        type="text"
+        placeholder="Email Address"
+      />
+      <input
+        className="border border-red-600 rounded-lg p-2 "
+        type="text"
+        placeholder="Password"
       />
       <button
         onClick={() => alert("Don't Click me")}
-        className="bg-red-600 rounded-2xl p-2 text-white">
-        Click Me
+        className="bg-red-600 rounded-full p-2 text-white">
+        Sign Up
       </button>
     </div>
   );
